942e6bcd5d3a5d35aeb0ce03d81e8094af482020
Pratikbhai Patel <pratikpatel@me.com>
2017-02-07 13:04:18 -0500

    You get optionals, you get optionals, EVERYBODY GETS OPTIONALS!!!

--- FindAProtest/FindAProtest/Models/Event.swift
+++ FindAProtest/FindAProtest/Models/Event.swift
@@ -9,20 +9,20 @@
 import Foundation
 
 struct Event {
-    var id: Int
-    var movementId: Int
-    var categoryId: Int
-    var organizationId: Int
-    var name: String
-    var time: Date
-    var created: Date
-    var updated: Date
-    var city: String
-    var state: String
-    var location: String
-    var description: String
-    var tags: [String]
-    var link: URL
-    var estimatedSize: Int
-    var actualSize: Int
+    var id: Int?
+    var movementId: Int?
+    var categoryId: Int?
+    var organizationId: Int?
+    var name: String?
+    var time: Date?
+    var created: Date?
+    var updated: Date?
+    var city: String?
+    var state: String?
+    var location: String?
+    var description: String?
+    var tags: [String]?
+    var link: URL?
+    var estimatedSize: Int?
+    var actualSize: Int?
 }
