981a31bde1e7f09045951d3012159de84f8496e6
Pratikbhai Patel <pratikpatel@me.com>
2017-02-09 12:35:11 -0500

    Foundations for color theme with UIColor extension file Foundations for localizability with Localizable.strings file

---
+++ FindAProtest/FindAProtest/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Extensions.swift
+//  FindAProtest
+//
+//  Created by Pratikbhai Patel on 2/9/17.
+//  Copyright © 2017 Find a Protest. All rights reserved.
+//
+
+import Foundation
